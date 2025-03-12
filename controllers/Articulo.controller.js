eb022e527291dc18234b6f06f307bab7902970d3
Roger16118 <2321082555@alumno.utpuebla.edu.mx>
2025-03-12 10:28:36 -0600

    Hola

--- controllers/Articulo.controller.js
+++ controllers/Articulo.controller.js
@@ -57,4 +57,4 @@ module.exports = {
     deleteArticleById,
 }
 
-//Termine el trabajo 
+ 
--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/locations', locationRoute);
 
 
 // Realizar petición de conexión a MongoDB
-mongoose.connect('mongodb://localhost:27017/MyDatabase')
+mongoose.connect('mongodb+srv://RogerAlexis:Alexis161718@cluster0.2dwl5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster/BB')
 .then(() => {
     console.log('Se estableció la conexión a base de datos exitosamente');
     app.listen(3000, () => {
